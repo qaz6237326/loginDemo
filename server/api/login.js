a9606acfddb68369c8fdf0aaf1e1f7115c3ef2ac
qinwl <qinwl@163.com>
2018-12-17 14:19:09 +0800

    优化代码

--- server/api/login.js
+++ server/api/login.js
@@ -8,54 +8,20 @@ const config = require('../../config/config.json');
 const secretOrKey = config.cfg.secretOrKey;
 const utf8 = config.httpHeader.utf8;
 
-const databaseName = 'wldb';
+const databaseNameSql = 'use wldb';
+const queryUser = `SELECT id,name,age FROM user WHERE name= ? and password= ?`;
 
 function Request(app) {
-    app.post('/loginApi', urlencodeParser, function(req, res) {
-        res.writeHead(200, utf8);//设置response编码为utf-8
+    app.post('/loginApi', (req, res) => {
         // 接收到formData表单的数据,form表单的数据在request.body里
         // console.log(req.body);
-
-        var json = {};
-        var body = {};
-
-        // 用于接收json数据，data为Buffer类型，需要toString()转换，然后用JSON.parse()转换成JSON数据
-        req.on('data', function(data) {
-            body = JSON.parse(data.toString());
-        });
-
-        req.on('end', function() {
-            if (!body.name || !body.password) {
-                json = {
-                    result: false,
-                    message: '请填写完整的信息'
-                };
-                res.end(JSON.stringify(json));
-            } else {
-                loginDB(body.name, body.password)
-                    .then(function(data) {
-                        if (data) {
-                            json = {
-                                result: true,
-                                message: '登录成功',
-                                data: data
-                            };
-                        } else {
-                            json = {
-                                result: false,
-                                message: '账号或密码错误！'
-                            };
-                        }
-                        res.end(JSON.stringify(json));
-                    });
-            }
-        });
-    });
-    app.post('/current', (req, res) => {
         res.writeHead(200, utf8);//设置response编码为utf-8
+
         // 定义规则
         let rule = {};
-        var body;
+        let body;
+
+        // 用于接收json数据，data为Buffer类型，需要toString()转换，然后用JSON.parse()转换成JSON数据
         req.on('data', function(data) {
             body = JSON.parse(data.toString());
             // 规则头使用name作为内容
@@ -71,19 +37,29 @@ function Request(app) {
                 };
                 res.end(JSON.stringify(json));
             } else {
+                // 查询数据库是否有相关用户信息
                 loginDB(body.name, body.password)
                     .then(data => {
                         if (data) {
-                            jwt.sign(rule, secretOrKey, {expiresIn: 20}, (err, token) => {
-                                if (err) return console.log(err);
-                                json = {
-                                    result: true,
-                                    message: '获取token成功',
-                                    data: data,
-                                    token: 'Bearer ' + token // 需要固定一个头部的名字 'Bearer '
-                                };
-                                res.end(JSON.stringify(json));
-                            });
+                            // 创建token
+                            createJwt(rule, 20)
+                                .then(function (token) {
+                                    if (token) {
+                                        json = {
+                                            result: true,
+                                            message: '获取token成功',
+                                            data: data,
+                                            token: 'Bearer ' + token // 需要固定一个头部的名字 'Bearer '
+                                        };
+                                        res.end(JSON.stringify(json));
+                                    }
+                                })
+                        } else {
+                            json = {
+                                result: false,
+                                message: '账号或密码错误！'
+                            };
+                            res.end(JSON.stringify(json));
                         }
                     })
             }
@@ -91,7 +67,7 @@ function Request(app) {
     });
     app.get('/ver', passport.authenticate('jwt', { session: false }), function(req, res) {
         res.writeHead(200, utf8);//设置response编码为utf-8
-        console.log('校验成功')
+        console.log('校验成功');
         var json = {
             result: true,
             message: '校验token成功'
@@ -100,15 +76,38 @@ function Request(app) {
     })
 }
 
+/**
+ * 创建一个token
+ * @param rule 是任意对象，用来制定规则
+ * @param time token过期的时间
+ * */
+function createJwt(rule,time) {
+    return new Promise(function(resolve, reject) {
+        jwt.sign(rule, secretOrKey, {expiresIn: time}, (err, token) => {
+            if (err) {
+                console.log(err);
+                return resolve(false);
+            }
+            resolve(token);
+        });
+    })
+
+}
+
+/**
+ * 登录查询数据库
+ * @param name 用户名
+ * @param password 密码
+ * */
 function loginDB(name, password) {
     // 使用Promise处理异步
     return new Promise(function(resolve, reject) {
         // 选择需要查询的数据库
-        db.query(`use ${databaseName}`, function(err, result) {
+        db.query(databaseNameSql, function(err, result) {
             if (err) return console.log(err);
 
             // 查询表里的用户名和密码是否匹配，如果匹配返回用户数据，不匹配返回false
-            db.query(`SELECT id,name,age FROM user WHERE name='${name}' and password='${password}'`, function(err, result) {
+            db.query(queryUser, [name, password], function(err, result) {
                 if (err) return console.log(err);
                 if (result.length) {
                     resolve(result);
@@ -125,6 +124,6 @@ function loginDB(name, password) {
 }
 
 
-module.exports = api ={
+module.exports ={
     Request: Request
 };
