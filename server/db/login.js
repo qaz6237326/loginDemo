96a6fc26244cd63bef9b7b1119dfc243db3d9de0
qinwl <qinwl@163.com>
2018-12-20 18:13:33 +0800

    优化user代码

--- server/db/login.js
+++ server/db/login.js
@@ -14,20 +14,23 @@ module.exports = {
         return new Promise(function (resolve, reject) {
             // 选择需要查询的数据库
             db.query('use ' + databaseName, function (err, result) {
-                if (err) return console.log(err);
-
-                // 查询表里的用户名和密码是否匹配，如果匹配返回用户数据，不匹配返回false
-                db.query(queryUser, [name, password], function (err, result) {
-                    console.log(name + '用户的数据---------------------------------');
-                    if (err) return console.log(err);
-                    if (result.length) {
-                        resolve(result);
-                    } else {
-                        resolve(false);
-                    }
-                    console.log(result);
-                    console.log('查询到的数据---------------------------------');
-                })
+                if (err) {
+                    resolve(false);
+                    return console.log(err);
+                } else {
+                    // 查询表里的用户名和密码是否匹配，如果匹配返回用户数据，不匹配返回false
+                    db.query(queryUser, [name, password], function (err, result) {
+                        console.log(name + '用户的数据---------------------------------');
+                        if (err) return console.log(err);
+                        if (result.length) {
+                            resolve(result);
+                        } else {
+                            resolve(false);
+                        }
+                        console.log(result);
+                        console.log('查询到的数据---------------------------------');
+                    })
+                }
             })
         });
     }
